7de141e423307d22875fd479e17c1f7aab8303ed
daeseungmoon <tmdeoans@snu,ac,jr>
2022-12-22 15:47:48 +0900

    Revert "reset --hard"
    This reverts commit 9046d1812aacd66cf7e935b68000bff073a92275.

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -3,25 +3,27 @@
   import autoAnimate from "$lib/auto-animate"
   import HeroImage from "$lib/notion2svelte/HeroImage.svelte"
   let posts = $page.data.posts
-  let layout: "card" | "row" = "row"
-  const handleToggle = () => {
-    layout = layout === "card" ? "row" : "card"
+  let toggle = false
+
+  $: {
+    ;[toggle]
+    posts = [...posts].reverse()
   }
 </script>
 
 <section class="flex flex-col flex-grow">
   <div class="pb-4 border-b mt-4 lg:mt-0">BLOG</div>
-  <button class="btn" on:click={handleToggle}>toggle</button>
+  <button class="btn" on:click={() => (toggle = !toggle)}>toggle</button>
   <ul class="card-list" use:autoAnimate>
     {#each posts as { title, description, time, slug, image, hashtags } (slug)}
       {@const href = "/blog/" + slug}
-      <li id={slug} class="card">
+      <li
+        id={slug}
+        class="flex flex-col w-full flex-grow md:max-w-[320px] border shadow-md bg-white"
+      >
         <article>
           <a {href}>
-            <HeroImage
-              src={image.url}
-              height={layout === "card" ? "200px" : "150px"}
-            />
+            <HeroImage src={image.url} height="200px" />
           </a>
           <div class="px-6 py-7 h-[360px] flex flex-col">
             <div class="flex gap-3 mt-6 justify-center">
@@ -29,7 +31,7 @@
                 <div class="badge badge-outline">{name}</div>
               {/each}
             </div>
-            <h2 class="mt-4 text-[22px] leading-7 text-center">
+            <h2 class="mt-4">
               <a {href}>
                 {title}
               </a>
@@ -37,7 +39,7 @@
             <time class="mt-2 flex justify-end text-xs text-base-300">
               {time}
             </time>
-            <p class="mt-5 text-center description">
+            <p class="mt-5 text-center">
               {description}
             </p>
             <a {href} class="btn btn-primary rounded-2xl mx-auto mt-auto">
@@ -51,34 +53,28 @@
 </section>
 
 <style>
-  .description {
+  h2 {
+    @apply text-[22px] leading-7 text-center;
+  }
+
+  p {
     @apply h-[100px] overflow-hidden text-ellipsis;
     display: -webkit-box;
     -webkit-line-clamp: 4;
     -webkit-box-orient: vertical;
   }
 
-  .row {
-    @apply flex w-full flex-grow  border shadow-md;
+  .card-list {
+    @apply flex flex-wrap relative w-full gap-10 mt-9 mx-auto;
+    /* gap-9 */
   }
 
-  .card {
-    @apply flex flex-col w-full flex-grow  border shadow-md;
-  }
-
-  @media (min-width: 768px) {
-    .card {
-      max-width: 320px;
+  /* @media (min-width: 447px) {
+    .card-list {
+      max-width: 415px;
     }
   }
-
-  .card-list {
-    @apply flex flex-wrap relative w-full gap-10 mt-9 mx-auto;
-  }
-
-  .card-list {
-    @apply flex flex-wrap relative w-full gap-10 mt-9 mx-auto;
-  }
+*/
 
   /* 카드가 두줄일때 */
   @media (max-width: 1088px) {
